b3cc1079055e82c57d5ea4b86c5944f544298424
Ivan Jacob Agaloos Pesigan <jeksterslab@users.noreply.github.com>
2025-01-18 17:13:32 -0500

    init

--- .setup/scripts/clone.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-git clone git@github.com:jeksterslab/bootStateSpace.git
-rm -rf "$PWD.git"
-mv bootStateSpace/.git "$PWD"
-rm -rf bootStateSpace
--- tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(bootStateSpace)
-
-test_check("bootStateSpace")
